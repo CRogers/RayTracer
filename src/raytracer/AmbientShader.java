d57e59c9e3675d6b90ddd5010178feca72f6304e
Callum Rogers <callumrogers@hotmail.co.uk>
2011-11-18 22:21:21 +0000

    Allowed AmbientShader to take an arbitary value for intensity

--- src/raytracer/AmbientShader.java
+++ src/raytracer/AmbientShader.java
@@ -6,9 +6,15 @@ import processing.core.PVectorD;
 
 public class AmbientShader implements Shader {
 
+	double intensity;
+	
+	public AmbientShader(double intensity){
+		this.intensity = intensity;
+	}
+	
 	@Override
 	public double computeIntensity(List<LightSource> lightSources, PVectorD point, Item item) {
-		return 1;
+		return intensity;
 	}
 
 }
