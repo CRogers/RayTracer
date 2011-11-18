c32ff62cc6d249495422f39ad1ed3e72aaed1f3a
Callum Rogers <callumrogers@hotmail.co.uk>
2011-11-18 18:15:55 +0000

    Added first quick and dirty renderer

--- src/raytracer/Item.java
+++ src/raytracer/Item.java
@@ -3,7 +3,7 @@ package raytracer;
 import java.util.*;
 import processing.core.*;
 
-public abstract class Item {
+public abstract class Item { 
 
 	public abstract boolean isIntersectionPoint(PVectorD p);
 	public abstract PVectorD[] intersectionPoints(Line l);
--- src/raytracer/RayTracer.java
+++ src/raytracer/RayTracer.java
@@ -1,17 +1,46 @@
 package raytracer;
 
-import processing.core.PApplet;
+import java.util.*;
+
+import processing.core.*;
 
 
 public class RayTracer extends PApplet {
-
+	
+	private List<Item> items = new ArrayList<Item>();
+	
 	public void setup() {
-		size(800,600,JAVA2D);
+		size(600,600,JAVA2D);
 		smooth();
 		noLoop();
+		
+		items.add(new Sphere(new PVectorD(0,0,3), 1));
 	}
 
 	public void draw() {
 		background(0);
+		castRays();
+	}
+	
+	private void castRays(){
+		
+		double dh = (double)height;
+		double dw = (double)width;
+		
+		for(int x = -width/2; x < width/2; x++){
+			for(int y = -height/2; y < height/2; y++){
+				Line ray = new Line(PVectorD.zero(), new PVectorD(x/dw, y/dh, 1));
+				
+				for(Item item : items){
+					PVectorD[] ips = item.intersectionPoints(ray);
+					
+					if(ips.length > 0){
+						set(width/2+x,height/2+y,color(255));
+					}
+				}
+				
+			}
+		}
+		
 	}
 }
